d49a58b8faae969eeae20e66567e340e9ff35c54
Alexandre Faure <alexandre.faure@student-cs.fr>
2025-08-10 14:50:40 +0200

    Removed useless routes

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -4,10 +4,6 @@ import notificationsRoutes from './notifications';
 
 const router = Router();
 
-router.get('/', (_, res) => {
-    res.json({ message: 'Welcome to the Spark backend API!' });
-});
-
 router.use('/images', imagesRoutes);
 router.use('/notifications', notificationsRoutes);
 
